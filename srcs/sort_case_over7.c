e129de8519c9b439b8bfff9ce3b01c981c4afe93
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-03-01 01:11:10 +0900

    [fix]:& 0 >> & 1

--- srcs/sort_case_over7.c
+++ srcs/sort_case_over7.c
@@ -10,12 +10,14 @@ void	sort_case_over7(t_info *info)
 	i = 1;
 	k = 0;
 	pb_count = 0;
-	while (i < 100)
+	while (i < 16)
 	{
-		j = info->argc;
+		j = info->argc - 1;
 		while (j > 0)
 		{
-			if (((info->stack.compression_a[j] >> k) & i) == 1)
+			printf("\x1b[36m[j = %d, compression[j] = %d, k = %d, i = %d]\n\033[m", j, info->stack.compression_a[j], k , i);
+			printf("\x1b[36m[%d]\n\033[m", ((info->stack.compression_a[j] >> k) & 1));
+			if (((info->stack.compression_a[j] >> k) & 1) == 0)
 			{
 				pb(info);
 				pb_count++;
@@ -24,13 +26,11 @@ void	sort_case_over7(t_info *info)
 				ra(info);
 			j--;
 		}
-		stack_debug(info);
 		while (pb_count > 0)
 		{
 			pa(info);
 			pb_count--;
 		}
-		stack_debug(info);
 		i *= 2;
 		k++;
 	}
