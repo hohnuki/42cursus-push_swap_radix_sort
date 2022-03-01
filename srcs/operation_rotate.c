d312bc615f29d42c05722039913554f585cda5de
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-03-01 16:39:08 +0900

    [feat]:簡単な基数ソート完成

--- srcs/operation_rotate.c
+++ srcs/operation_rotate.c
@@ -7,15 +7,17 @@ void	ra(t_info *info)
 	int	i;
 
 	printf("ra \n");
-	i = info->stack.head_a - 1;
+	i = info->stack.head_a;
+//	printf("\x1b[36m[i = %d]\n\033[m", i);
 	tmp_a = info->stack.a[i];
+//	printf("\x1b[36m[tmp_a = %d]\n\033[m", tmp_a);
 	while (i > 0)
 	{
 		info->stack.a[i] = info->stack.a[i - 1];
 		i--;
 	}
 	info->stack.a[0] = tmp_a;
-	i = info->stack.head_a - 1;
+	i = info->stack.head_a;
 	tmp_compression_a = info->stack.compression_a[i];
 	while (i > 0)
 	{
--- srcs/sort_case_over7.c
+++ srcs/sort_case_over7.c
@@ -10,14 +10,13 @@ void	sort_case_over7(t_info *info)
 	i = 1;
 	k = 0;
 	pb_count = 0;
-	while (i < 16)
+	while (i < 600)
 	{
 		j = info->argc - 1;
 		while (j > 0)
 		{
-			printf("\x1b[36m[j = %d, compression[j] = %d, k = %d, i = %d]\n\033[m", j, info->stack.compression_a[j], k , i);
-			printf("\x1b[36m[%d]\n\033[m", ((info->stack.compression_a[j] >> k) & 1));
-			if (((info->stack.compression_a[j] >> k) & 1) == 0)
+//			printf("\x1b[36m[j = %d, compression[info->stack.head_a] = %d, a[info->stack.head_a] = %d, k = %d, info->stack.head_a = %d]\n\033[m", j, info->stack.compression_a[info->stack.head_a], info->stack.a[info->stack.head_a], k, info->stack.head_a);
+			if (((info->stack.compression_a[info->stack.head_a] >> k) & 1) == 0)
 			{
 				pb(info);
 				pb_count++;
