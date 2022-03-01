10ee66f3cd8ccbb6af8738858b295f3949fe708d
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-03-01 22:25:12 +0900

    [feat]:rrbを追加

--- srcs/operation_reverse_rotate.c
+++ srcs/operation_reverse_rotate.c
@@ -27,3 +27,31 @@ void	rra(t_info *info)
 	}
 	info->stack.compression_a[info->stack.head_a] = tmp_compression_a;
 }
+
+void	rrb(t_info *info)
+{
+	int	tmp_b;
+	int	tmp_compression_b;
+	int	i;
+
+	printf("rrb\n");
+	i = 0;
+//	printf("\x1b[36m[i = %d]\n\033[m", i);
+	tmp_b = info->stack.b[0];
+//	printf("\x1b[36m[tmp_b = %d]\n\033[m", tmp_b);
+	while (i < info->stack.head_b)
+	{
+		info->stack.b[i] = info->stack.b[i + 1];
+		i++;
+	}
+	info->stack.b[info->stack.head_b] = tmp_b;
+
+	i = 0;
+	tmp_compression_b = info->stack.compression_b[0];
+	while (i < info->stack.head_b)
+	{
+		info->stack.compression_b[i] = info->stack.compression_b[i + 1];
+		i++;
+	}
+	info->stack.compression_b[info->stack.head_b] = tmp_compression_b;
+}
